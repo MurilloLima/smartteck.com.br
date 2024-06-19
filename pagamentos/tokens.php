dab0aa79da4261bda25371a4bc64532576f8c420
Murillo Lima <contato@murillolimadev.com.br>
2024-06-19 16:12:50 -0300

    token ok

--- pagamentos/tokens.php
+++ pagamentos/tokens.php
@@ -1,4 +1,4 @@
 <?php 
 $access_token = 'APP_USR-88375949092467-061413-6be02942d7bd02a68191950badfc3bd3-1856414377';
-$public_key     = 'APP_USR-88375949092467-061413-6be02942d7bd02a68191950badfc3bd3-1856414377';
+$public_key     = 'APP_USR-e8447d69-83d1-4957-b1d2-a3fa595324d7';
  ?>
